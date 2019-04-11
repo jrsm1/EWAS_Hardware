cf44fda1df01fd65fcf6b7823d016c6756f20a96
christiancancel <christian.cancel@upr.edu>
2019-04-11 07:21:53 -0400

    Update 1_Hz_Timer.c
    Made changes so that by the press of the button S1 of the launchpad, sets P2.4 to logic high and after 500ms via timer, sets P2.4 to logic low.

--- 1_Hz_Timer.c
+++ 1_Hz_Timer.c
@@ -5,7 +5,7 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
 /* Application Defines  */
-#define TIMER_PERIOD    0x8000
+#define TIMER_PERIOD    0x4000
 
 /* Timer_A UpMode Configuration Parameter */
 const Timer_A_UpModeConfig upConfig =
@@ -22,21 +22,19 @@ int main(void)
 {
     /* Stop WDT  */
     MAP_WDT_A_holdTimer();
-
     /* Configure ACLK */
     MAP_CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
-
-    /* Configuring P1.6 & 7 as output */
-    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN6);
-    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN6);
+    /* Configuring P2.4 as output */
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN4);
+    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN4);
+    MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
     /* Configuring Timer_A1 for Up Mode */
     MAP_Timer_A_configureUpMode(TIMER_A1_BASE, &upConfig);
-
     /* Enabling interrupts and starting the timer */
     MAP_Interrupt_enableSleepOnIsrExit();
-    MAP_Interrupt_enableInterrupt(INT_TA1_0);
-    MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);
-    
+    MAP_Interrupt_enableInterrupt(INT_PORT1);
     /* Enabling MASTER interrupts */
     MAP_Interrupt_enableMaster();  
 
@@ -47,9 +45,27 @@ int main(void)
     }
 }
 
+/* Port1 ISR - This ISR will progressively step up the duty cycle of the PWM
+ * on a button press
+ */
+void PORT1_IRQHandler(void)
+{
+    uint32_t status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P1);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, status);
+    if (status & GPIO_PIN1)
+    {
+        MAP_Interrupt_enableInterrupt(INT_TA1_0);
+        MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);
+        MAP_Timer_A_clearCaptureCompareInterrupt(TIMER_A1_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
+        MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN4);
+    }
+    MAP_GPIO_disableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+}
+
 void TA1_0_IRQHandler(void)
 {
-    MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN6);
-    MAP_Timer_A_clearCaptureCompareInterrupt(TIMER_A1_BASE,
-            TIMER_A_CAPTURECOMPARE_REGISTER_0);
+    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN4);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_Interrupt_disableInterrupt(INT_TA1_0);
+    MAP_Timer_A_stopTimer(TIMER_A1_BASE);
 }
