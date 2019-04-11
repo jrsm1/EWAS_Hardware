815d8fee373fdfeeb880e7abc95330046f88a867
christiancancel <christian.cancel@upr.edu>
2019-04-11 09:26:51 -0400

    Update 1_Hz_Timer.c

--- 1_Hz_Timer.c
+++ 1_Hz_Timer.c
@@ -54,18 +54,20 @@ void PORT1_IRQHandler(void)
     MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, status);
     if (status & GPIO_PIN1)
     {
-        MAP_Interrupt_enableInterrupt(INT_TA1_0);
-        MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);
-        MAP_Timer_A_clearCaptureCompareInterrupt(TIMER_A1_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
         MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN4);
     }
+    MAP_Interrupt_enableInterrupt(INT_TA1_0);
+    MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);
+    MAP_Timer_A_clearCaptureCompareInterrupt(TIMER_A1_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
     MAP_GPIO_disableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_Interrupt_enableInterrupt(INT_PORT1);
 }
 
 void TA1_0_IRQHandler(void)
 {
     MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN4);
     MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_Timer_A_clearCaptureCompareInterrupt(TIMER_A1_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
     MAP_Interrupt_disableInterrupt(INT_TA1_0);
     MAP_Timer_A_stopTimer(TIMER_A1_BASE);
 }
